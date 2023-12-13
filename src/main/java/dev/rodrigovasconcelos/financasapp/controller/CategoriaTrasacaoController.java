b53187e22bb7084d5245877c8bcdb4ae652ba751
Rodrigo GrauU <rodrigoassis1997@gmail.com>
2023-12-13 14:27:50 -0300

    fix: get user info to find results

--- src/main/java/dev/rodrigovasconcelos/financasapp/controller/CategoriaTrasacaoController.java
+++ src/main/java/dev/rodrigovasconcelos/financasapp/controller/CategoriaTrasacaoController.java
@@ -1,11 +1,23 @@
 package dev.rodrigovasconcelos.financasapp.controller;
 
 import dev.rodrigovasconcelos.financasapp.dto.CategoriaTransacaoDto;
+import dev.rodrigovasconcelos.financasapp.entity.Usuario;
 import dev.rodrigovasconcelos.financasapp.service.CategoriaTransacaoService;
+import dev.rodrigovasconcelos.financasapp.service.impl.UsuarioServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
 
@@ -15,11 +27,12 @@ import java.util.Set;
 public class CategoriaTrasacaoController {
 
     private CategoriaTransacaoService categoriaTransacaoService;
+    private UsuarioServiceImpl usuarioService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Set<CategoriaTransacaoDto> listar() {
-        Long usuarioId = 1L;
-        return categoriaTransacaoService.listar(usuarioId);
+        Usuario usuario = usuarioService.findUserByUsername(getUsername());
+        return categoriaTransacaoService.listar(usuario.getId());
     }
 
     @PostMapping
@@ -37,5 +50,10 @@ public class CategoriaTrasacaoController {
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long categoriaTransacaoId) {
         categoriaTransacaoService.remover(categoriaTransacaoId);
+    }
+
+    private String getUsername() {
+        SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
+        return securityContextHolderStrategy.getContext().getAuthentication().getName();
     }
 }
